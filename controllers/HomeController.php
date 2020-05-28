d9eaf7a3b2ebe8c86713ece5ccf92eb6533b2e58
Vladimir Drobyshev <dvvinfo@mail.ru>
2020-05-28 12:49:12 +0300

    made a block of Hot Offers

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace app\controllers;
 
+use app\models\Product;
+
 class HomeController extends AppController
 {
 
     public function actionIndex()
     {
-        return $this->render('index');
+        //для получения всех товаров из БД по ключу is_offer
+        $offers = Product::find()->where(['is_offer' => 1])->limit(4)->all();
+        return $this->render('index', compact('offers'));
     }
 
 }
