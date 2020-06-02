5aa6e98714e38aa6bf93d082d8f2670b038011fe
Vladimir Drobyshev <dvvinfo@mail.ru>
2020-06-02 12:22:33 +0300

    added a shopping cart

--- web/js/main.js
+++ web/js/main.js
@@ -65,3 +65,77 @@ paypal.minicart.cart.on('checkout', function (evt) {
         evt.preventDefault();
     }
 });
+
+/* Cart */
+
+function showCart(cart){
+    $('#modal-cart .modal-body').html(cart);
+    $('#modal-cart').modal();
+    let cartSum = $('#cart-sum').text() ? $('#cart-sum').text() : '$0';
+    if(cartSum){
+        $('.cart-sum').text(cartSum);
+    }
+}
+
+function getCart(){
+    $.ajax({
+        url: 'cart/show',
+        type: 'GET',
+        success: function (res) {
+            if(!res) alert('Ошибка');
+            showCart(res);
+        },
+        error: function(){
+            alert('Error!');
+        }
+    });
+}
+
+function clearCart() {
+    $.ajax({
+        url: 'cart/clear',
+        type: 'GET',
+        success: function (res) {
+            if(!res) alert('Ошибка');
+            showCart(res);
+        },
+        error: function(){
+            alert('Error!');
+        }
+    });
+}
+
+$('.add-to-cart').on('click', function () {
+    let id = $(this).data('id');
+    $.ajax({
+        url: 'cart/add',
+        data: {id: id},
+        type: 'GET',
+        success: function (res) {
+            if(!res) alert('Ошибка');
+            showCart(res);
+        },
+        error: function(){
+            alert('Error!');
+        }
+    });
+    return false;
+});
+
+$('#modal-cart .modal-body').on('click', '.del-item', function () {
+    let id = $(this).data('id');
+    $.ajax({
+        url: 'cart/del-item',
+        data: {id: id},
+        type: 'GET',
+        success: function (res) {
+            if(!res) alert('Ошибка');
+            showCart(res);
+        },
+        error: function(){
+            alert('Error!');
+        }
+    });
+});
+
+/* Cart */
